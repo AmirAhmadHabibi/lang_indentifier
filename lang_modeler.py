9491085a989ecb1a731532a91172ba4bcc13525b
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2018-09-18 10:28:45 -0600

    added lang_modeler

---
+++ lang_modeler.py
@@ -0,0 +1,27 @@
+class LangModeler:
+    def __init__(self, corpus_name, corpus, n=4):
+        self.name = corpus_name
+        self.corpus = ' ' * n + corpus + ' ' * n
+        self.n = n
+        self.ngrams = {i: dict() for i in range(1, n + 1)}
+        self._count_ngrams()
+
+    def _count_ngrams(self):
+        """
+        this method counts the number of n-grams from 1 to n for the given corpus
+        and stores them into a dictionary mapping the numbers k (1 to n) to the
+        dictionaries of k-grams and their counts
+        the resulting dictionary would be like this:
+            self.ngrams[3]['the']=12
+        """
+        for i in range(self.n, len(self.corpus) - self.n):
+            for j in range(1, self.n + 1):
+                curr_ngram = self.corpus[i:i + j]
+                if curr_ngram not in self.ngrams[j]:
+                    self.ngrams[j][curr_ngram] = 1
+                else:
+                    self.ngrams[j][curr_ngram] += 1
+
+    def p(self, ngram):
+        # TODO
+        pass
