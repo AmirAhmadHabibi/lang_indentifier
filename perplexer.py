6440e5203dc1bf0a480a8060a65bd2696532e51f
anandh02 <anandhperumal02@gmail.com>
2018-09-20 10:44:55 -0600

    perplexer and main file update
    Reading of the file is still causing a issue need to be fixed

--- perplexer.py
+++ perplexer.py
@@ -1,4 +1,20 @@
-def perplexity(lang_model, test_set):
-    perp = 0
-    # TODO
-    return perp
+
+def pp(model,testcase, n_gram_value):
+    """
+    :param testcase: test corpus
+    :param n_gram_value: the ngram number ex: bigram =2 , trigram =3
+
+    """
+    N = 0
+    pp = 1
+
+    for j in range(0, len(testcase) ):
+        curr_ngram = testcase[j:j+n_gram_value]
+        s = model.p(curr_ngram)
+        # for unsmoothed value skipping the word
+        if s != 0 :
+            N += 1
+            pp = pp * (1/ s)
+    pp = pow(pp,1/N)
+    return pp
+
