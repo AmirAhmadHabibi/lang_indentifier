35679ce8959bbd5659ed1e4bd8a58687fa473168
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2018-09-22 19:01:00 -0600

    integrated all into langid

--- lang_modeler.py
+++ lang_modeler.py
@@ -1,19 +1,17 @@
 class LangModeler:
-    def __init__(self, name, corpus, n=4, smoothing='unsmoothed'):
+    def __init__(self, name, corpus, n=4):
         """
         :param name: the name of the corpus for future reference
         :param corpus: the text of the corpus
         :param n: size of the context in n-gram
-        :param smoothing: one of the following methods: {"unsmoothed", "laplace", "interpolation"}
         """
         self.name = name
         self.corpus = ' ' * n + corpus + ' ' * n
         self.n = n
         self.corpus_len = len(corpus)
         self.counts = [dict() for _ in range(n + 1)]
-        self.smoothing = smoothing
         self.v = 0  # length of the vocabulary
-        self.l = [0] * (n + 1)  # lambda values for deleted interpolation alg
+        self.l = [[]] * (n + 1)  # lambda values for deleted interpolation alg
 
         self._count_ngrams()
         self._set_lambdas()
@@ -38,65 +36,66 @@ class LangModeler:
 
     def _set_lambdas(self):
         """
-        calculate the lambda values for deleted interpolation algorithm.
+        calculate the lambda values for deleted interpolation algorithm. for each max ngram size
         """
-        # for all the ngrams of size n
-        for ngram in self.counts[self.n].keys():
-            max_val = 0
-            max_i = 0
-            for i in range(self.n):
-                sub_ngram = ngram[i:]
-                try:
-                    if len(sub_ngram) == 1:
-                        val = (self.counts[self.n - i][sub_ngram] - 1) / (self.corpus_len - 1)
-                    else:
-                        val = (self.counts[self.n - i][sub_ngram] - 1) / (
-                                    self.counts[self.n - i - 1][sub_ngram[:-1]] - 1)
-                except ZeroDivisionError:
-                    val = 0
-                except KeyError:  # Need to investiage
-                    return 0
+        for size in range(1, self.n + 1):
+            lmbd = [0] * (size + 1)
+            for ngram in self.counts[size].keys():
+                max_val = 0
+                max_i = 0
+                for i in range(size):
+                    sub_ngram = ngram[i:]
+                    try:
+                        if len(sub_ngram) == 1:
+                            val = (self.counts[size - i][sub_ngram] - 1) / (self.corpus_len - 1)
+                        else:
+                            val = (self.counts[size - i][sub_ngram] - 1) / (
+                                    self.counts[size - i - 1][sub_ngram[:-1]] - 1)
+                    except ZeroDivisionError:
+                        val = 0
+                    except KeyError:  # Need to investiage
+                        return 0
 
-                if val > max_val:
-                    max_val = val
-                    max_i = i
+                    if val > max_val:
+                        max_val = val
+                        max_i = i
 
-            self.l[self.n - max_i] += self.counts[self.n][ngram]
+                lmbd[size - max_i] += self.counts[size][ngram]
 
-        # normalise lambdas
-        sum_l = sum(self.l)
-        self.l = [el / sum_l for el in self.l]
+            # normalise lambdas
+            sum_l = sum(lmbd)
+            self.l[size] = [el / sum_l for el in lmbd]
 
     def _calculate_prob(self, ngram, k=0):
         # TODO : zero counts
         try:
+            size = len(ngram)
             # if its a unigram divide by the size of the vocabulary otherwise divide by count of the previous tokens
-            if self.n == 1:
-                return (self.counts[self.n][ngram] + k) / (self.corpus_len + (k * self.v))
+            if size == 1:
+                return (self.counts[size][ngram] + k) / (self.corpus_len + (k * self.v))
             else:
-                return (self.counts[self.n][ngram] + k) / (self.counts[self.n - 1][ngram[:-1]] + (k * self.v))
+                return (self.counts[size][ngram] + k) / (self.counts[size - 1][ngram[:-1]] + (k * self.v))
         except ZeroDivisionError:
             return 0
         except KeyError:
             return 0
 
-    def p(self, ngram):
+    def p(self, ngram, smoothing='unsmoothed'):
         """
         returns the probability of last token of the input ngram assuming
         the previous tokens had happened.
         """
         # TODO: log probability?
-        if len(ngram) != self.n:
-            raise Exception("Ngram size mismatch!")
 
         prob = 0
-        if self.smoothing == 'unsmoothed':
+        if smoothing == 'unsmoothed':
             prob = self._calculate_prob(ngram)
-        elif self.smoothing == 'laplace':
+        elif smoothing == 'laplace':
             prob = self._calculate_prob(ngram, k=1)
-        elif self.smoothing == 'interpolation':
-            for i in range(self.n):
-                prob += self._calculate_prob(ngram[i:]) * self.l[i + 1]
+        elif smoothing == 'interpolation':
+            size = len(ngram)
+            for i in range(size):
+                prob += self._calculate_prob(ngram[i:]) * self.l[size][i + 1]
         else:
             raise Exception("Smoothing method not defined!")
 
--- langid.py
+++ langid.py
@@ -1,6 +1,8 @@
 import pickle
+from time import time
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelEncoder
 from lang_modeler import LangModeler
-import perplexer as p
 import os
 import sys
 
@@ -8,37 +10,124 @@ TRAIN_PATH = './811_a1_train/'
 
 
 class LangIder:
-    def __init__(self):
-        self.models = dict()
-        for method in ['unsmoothed', 'laplace', 'interpolation']:
-            with open('./models/' + method + '.pkl', 'rb') as infile:
-                self.models[method] = pickle.load(infile)
-        # # build all models
-        # self.models = []
-        # for filename in os.listdir(TRAIN_PATH):
-        #     with open(TRAIN_PATH + filename, 'r') as f:
-        #         model = LangModeler(filename, f.read().replace('\n', ' '), ngram_size, method)
-        #         self.models.append(model)
+    def __init__(self, max_n):
+        self.max_n = max_n
+        self.models = []
+        self.smoothing_methods = ['unsmoothed', 'laplace', 'interpolation']
+        if os.path.isfile('./models/models_' + str(max_n) + '.pkl'):
+            with open('./models/models_' + str(max_n) + '.pkl', 'rb') as infile:
+                self.models = pickle.load(infile)
+            print('[log] language models loaded')
+        else:
+            self._train_models(max_n)
 
-    def predict(self, path, method):
+        if os.path.isfile('./predictions/opt_parameters.pkl'):
+            with open('./predictions/opt_parameters.pkl', 'rb') as infile:
+                self.opt_param = pickle.load(infile)
+            print('[log] optimised parameters loaded')
+        else:
+            self._optimize_parameter()
+
+    def _train_models(self, max_ngram_size):
+        print('[log] training...')
+        s_time = time()
+
+        # build models for all languages and each n value up to max_ngram_size
+        for filename in sorted(os.listdir(TRAIN_PATH)):
+            with open(TRAIN_PATH + filename, 'r') as infile:
+                model = LangModeler(name=filename, corpus=infile.read().replace('\n', ' '), n=max_ngram_size)
+                self.models.append(model)
+
+        os.makedirs('./models/', exist_ok=True)
+        with open('./models/models_' + str(max_ngram_size) + '.pkl', 'wb') as outfile:
+            pickle.dump(self.models, outfile)
+
+        print('[log] training done in', round(time() - s_time), 's')
+
+    def _optimize_parameter(self):
+        # find the best ngram size for each method
+        self.opt_param = dict()
+        for method in self.smoothing_methods:
+            # predict for each size
+            best_size = 0
+            best_fscore = -1
+            for size in range(1, self.max_n + 1):
+                print('[log] predicting', method, size)
+                self.predict('./811_a1_dev/', method, size)
+
+                # evaluate the prediction
+                f_s = LangIder.f_scorer(method + '_' + str(size))
+                if f_s >= best_fscore:
+                    best_fscore = f_s
+                    best_size = size
+            print('[log] best size for', method, 'is', best_size, 'with f-score of', best_fscore)
+            self.opt_param[method] = best_size
+
+        with open('./predictions/opt_parameters.pkl', 'wb') as outfile:
+            pickle.dump(self.opt_param, outfile)
+
+    def predict(self, path, method, size=None):
+        if size is None:
+            size = self.opt_param[method]
         output = ''
-        size = self.models[method][0].n
-
-        # read all test cases in the given path
-        for filename in os.listdir(path):
+        # for each files in the given path find the lowest perplexity
+        for filename in sorted(os.listdir(path)):
             with open(path + filename, 'r') as infile:
                 test = infile.read().replace('\n', ' ')
             min_perplexity = sys.maxsize
             best_match = ''
-            # find the min perplexity among all language models of the given smoothing method
-            for model in self.models[method]:
-                perplexity = p.pp(model=model, testcase=test, ngram_size=size)
+            # find the minimum perplexity among all language models of the given smoothing method
+            for model in self.models:
+                perplexity = self._perplex(model=model, testcase=test, ngram_size=size, smoothing=method)
                 if perplexity < min_perplexity:
                     min_perplexity = perplexity
                     best_match = model.name
             # add the most similar model name and the input test case to the output
-            output += filename + '  ' + best_match + '   ' + str(round(min_perplexity, 4)) + '  ' + str(size) + '\n'
+            output += filename + '\t' + best_match + '\t' + str(round(min_perplexity, 2)) + '\t' + str(size) + '\n'
 
         # write the list of all input test cases and their most similar language
-        with open('./perplexity/' + method + '_' + str(size), "w") as outfile:
+        os.makedirs('./predictions/', exist_ok=True)
+        with open('./predictions/' + method + '_' + str(size), 'w') as outfile:
             outfile.write(output)
+
+    @staticmethod
+    def f_scorer(filename):
+        y_true = []
+        y_pred = []
+        with open('./predictions/' + filename, 'r') as infile:
+            for line in infile:
+                dev, tra, p, s = line.split()
+                dev = dev.partition('-')[2].partition('.')[0]
+                tra = tra.partition('-')[2].partition('.')[0]
+                y_true.append(dev)
+                y_pred.append(tra)
+        le = LabelEncoder()
+        le.fit(y_true)
+        f_s = f1_score(le.transform(y_true), le.transform(y_pred), average='macro')
+        print('[log] F-score for', filename, 'is', f_s)
+        return f_s
+
+    @staticmethod
+    def _perplex(model, testcase, ngram_size, smoothing):
+        """
+        :param model: language model class
+        :param testcase: test corpus
+        :param ngram_size: the ngram number ex: bigram =2 , trigram =3
+        :param smoothing: one of the following methods: {"unsmoothed", "laplace", "interpolation"}
+
+        """
+        N = 0
+        pp = 1
+        testcase = ' ' * ngram_size + testcase + ' ' * ngram_size
+        for j in range(0, len(testcase) - ngram_size):
+            curr_ngram = testcase[j:j + ngram_size]
+            s = model.p(curr_ngram, smoothing)
+            if s != 0:
+                N += 1
+                pp = pp * (1 / s)
+        if N != 0:
+            pp = pow(pp, 1 / N)
+        return pp
+
+
+lid = LangIder(10)
